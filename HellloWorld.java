07b5da081b50c4a9966d0a0931582c3af7113c55
Jerry Zheng <jzhengaz@amazon.com>
2024-09-23 14:04:55 -0700

    update both

--- HellloWorld.java
+++ HellloWorld.java
@@ -2,6 +2,6 @@ class Test
 {
     public static void main(String []args)
     {
-        System.out.println("My First Java Progrbam.");
+        System.out.println("My First Java Progrfbam.");
     }
 };
